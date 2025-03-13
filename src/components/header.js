6651c87d17fc6f31d4c3803c1bca49a845a16a88
Jagan <166379843+JagadeshwaranK@users.noreply.github.com>
2025-03-13 18:44:09 +0530

    home-page updated

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,13 @@ import { Navbar, Container, Nav, Form, Button } from 'react-bootstrap';
 import '../index.css';
 import { Link } from 'react-router-dom';
 
+
 const Header = () => {
   return (
     <>
       <Navbar expand='lg' className='navbar navhead'>
         <Container>
-          <Navbar.Brand className='navhome text-white' href='#home'>AmmoNation</Navbar.Brand>
+          <Navbar.Brand className='navhome  text-white' href='#home'>AmmoNation</Navbar.Brand>
           <Form className='d-flex navb '>
             <Form.Control type='search' placeholder='Search' className='me-2' aria-label='Search' />
             <Button variant='outline-light' className='me-2'>Search</Button>
@@ -20,25 +21,28 @@ const Header = () => {
       </Navbar>
 
       <hr className='hr' />
-
-      <Nav className='justify-content-center navbar navitem'>
+        
+ <Nav className='justify-content-center navbar navitem'>
         <Nav.Item>
-          <Nav.Link href='#home'>Pistol</Nav.Link>
+          <Nav.Link as={Link} to='/handgun'>Handgun</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href='#about'>Ak47</Nav.Link>
+          <Nav.Link as={Link} to='/rifle'>Rifle</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href='#about'>Sniper</Nav.Link>
+          <Nav.Link as={Link} to='/shotgun'>Shotgun</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href='#about'>Vintage</Nav.Link>
+          <Nav.Link as={Link} to='/specialty'>Specialty</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href='#about'>Missile</Nav.Link>
+          <Nav.Link as={Link} to='/revolver'>Revolver</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href='#about'>Bom</Nav.Link>
+          <Nav.Link as={Link} to='/tactical'>Tactical</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to='/training'>Training</Nav.Link>
         </Nav.Item>
       </Nav>
     </>
